170f16899e142584a9788c39cc19eb9528a193b4
Truc Huynh <jackyhuynh87@gmail.com>
2021-02-26 09:34:01 -0500

    small change

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+
 
 namespace BackEnd2
 {
@@ -16,6 +11,7 @@ namespace BackEnd2
             CreateHostBuilder(args).Build().Run();
         }
 
+        // 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
